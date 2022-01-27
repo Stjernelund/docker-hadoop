94733667f94a85278a2dd98ee2a6a2dc63508356
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-01-27 13:51:39 +0100

    Update count_sum.py

--- dis_materials/count_sum.py
+++ dis_materials/count_sum.py
@@ -4,8 +4,8 @@ class MRCountSum(MRJob):
 
     def mapper(self, _, line):
         line = line.strip() # remove leading and trailing whitespace
-        if line.find("From:") == 0:
-            email_domain = line[line.find("@")+1:line.find(">")]
+        if line.find("From:") == 0 and line.find("@") != -1:
+            email_domain = line[line.find("@")+1:line.find(">") or line.find("[") or line.find("<") or line.find("]")]
             if len(email_domain) == 0:
                 email_domain == "empty"
             yield email_domain, 1
