3efadd361aa9142e13532d75bfdbfa94633bc7d3
Stjernelund <59571307+Stjernelund@users.noreply.github.com>
2022-01-27 15:07:23 +0100

    Update count_sum.py

--- dis_materials/count_sum.py
+++ dis_materials/count_sum.py
@@ -2,21 +2,13 @@ from mrjob.job import MRJob
 
 class MRCountSum(MRJob):
 
-    def mapper(line):
+    def mapper(self, _, line):
         line = line.strip() # remove leading and trailing whitespace
-        if line.find("From:") == 0 and line.find("@") != -1:
-            end_index = -1
-            email_domain = line[line.find("@")+1:end_index]
-            print(email_domain)
-            for i,char in enumerate(email_domain):
-                if char != '.' and char != '-' and not char.isnumeric() and not char.isalpha():
-                    print(char)
-                    print("her")
-                    email_domain = email_domain[0:i]
-                    break
+        if line.find("From:") == 0:
+            email_domain = line[line.find("@")+1:line.find(">")]
             if len(email_domain) == 0:
                 email_domain == "empty"
-            return email_domain, 1
+            yield email_domain, 1
 
     def combiner(self, key, values):
         yield key, sum(values)
